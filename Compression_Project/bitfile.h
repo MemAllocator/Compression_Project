84459fe29cd2367770f882270f7458beced0ced8
MemAllocator <omerkisoss@gmail.com>
2016-04-19 14:37:19 +0300

    test

--- Compression_Project/bitfile.h
+++ Compression_Project/bitfile.h
@@ -34,7 +34,7 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 ***************************************************************************/
-
+//testing commits
 #ifndef _BITFILE_H_
 #define _BITFILE_H_
 
